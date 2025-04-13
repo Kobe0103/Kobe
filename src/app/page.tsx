6d96d30813b2beb9d3f8120a43cb64460d5072c8
Kobe_Gaming <102873703+Kobe0103@users.noreply.github.com>
2025-04-14 00:26:17 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState, useEffect, useRef } from 'react'
 import emailjs from '@emailjs/browser'
-import { Analytics } from "@vercel/analytics/react"
 
 type Project = {
   title: string
